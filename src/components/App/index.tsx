28759b88d310fd31d8764135187d34c396d7d5d0
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-10 22:38:46 +0000

    Added app component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,23 @@
-import './style.css';
+import { useState } from "react";
+import List from "../List";
+import Input from "../Input";
+import "./style.css";
 
 function App() {
+  const [list, setList] = useState<(string | never)[]>([]);
+
+  function addItem(item: string) {
+    setList([...list, item]);
+  }
+
+  function removeItem(index: number) {
+    setList([...list.slice(0, index), ...list.slice(index + 1)]);
+  }
   return (
     <div className="App">
-      
+      <h1>To do App</h1>
+      <Input add={addItem} />
+      <List list={list} remove={removeItem} />
     </div>
   );
 }
